f562defac5bdb94a5d71a151296f9363797017a0
ConnOnGit <42461211+ConnOnGit@users.noreply.github.com>
2021-07-25 23:35:02 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@
 #Version: 1.1 - 20210306
 #branch="development"
 #OneLineInstaller: rm /tmp/install_ledb.sh > /dev/null  2>&1; wget https://raw.githubusercontent.com/connongit/led_driver/master/scripts/install.sh -qO /tmp/install_ledb.sh; chmod +x /tmp/install_ledb.sh; /tmp/./install_ledb.sh; rm /tmp/install_ledb.sh > /dev/null  2>&1 
-repo="https://github.com/splitti/phoniebox_led_control"
+repo="https://github.com/connongit/led_driver"
 branch="master"
 
 nocolor='\e[0m'
@@ -12,7 +12,7 @@ red="\e[1;91m"
 cyan="\e[1;36m"
 yellow="\e[1;93m"
 green="\e[1;92m"
-installPath="/home/pi/oled_phoniebox"
+installPath="/home/pi/led_driver"
 
 clear
 echo -e "/////////////////////////////////////////////////////////////////////////////////////////////////////////////"
@@ -38,7 +38,7 @@ echo -e "///${cyan}                         https://github.com/connongit/LED_dri
 echo -e "///                                                                                                         "
 echo -e "////////////////////////////////////////////////////////////////////////////////////////////////////////////"
 echo -e ""
-echo -e "${red}Please notice:${nocolor} This script will install a Service to drive  leds."
+echo -e "${red}Please notice:${nocolor} This script will install a Service to drive up to 5 leds."
 echo -e " "
 echo -e "Do you want to install this Service?"
 echo -e " "
@@ -65,7 +65,7 @@ echo -e "////////////////////////////////////////////////////////////////////"
 echo -e ""
 echo -e "Choose your type of animation:"
 echo -e " "
-options=("1 Knight Rider" "2 Star Trek" "Quit")
+options=("1 Knight Rider" "2 Permanent" "Quit")
 
 select opt in "${options[@]}"
 do
@@ -76,7 +76,7 @@ do
 			echo -e " "
             break
             ;;
-        "2 Star Trek")
+        "2 Permanent")
             echo -e ""
             ani_type="2"
 			echo -e " "
@@ -123,30 +123,30 @@ for p in ${packages[@]}; do
 	fi
 done
 echo -n -e "   --> Delete existing service:     "
-sudo service phoniebox_led_control stop > /dev/null 2>&1
-sudo systemctl disable phoniebox_led_control > /dev/null  2>&1
-sudo rm /etc/systemd/system/phoniebox_led_control.service > /dev/null  2>&1
-sudo rm -R ~/phoniebox_led_control > /dev/null  2>&1
+sudo service retrobox_led_control stop > /dev/null 2>&1
+sudo systemctl disable retrobox_led_control > /dev/null  2>&1
+sudo rm /etc/systemd/system/retrobox_led_control.service > /dev/null  2>&1
+sudo rm -R ~/retrobox_led_control > /dev/null  2>&1
 echo -e "${green}done${nocolor}";
 
 echo -n -e "   --> Create Directory:            "
-mkdir ~/phoniebox_led_control  > /dev/null  2>&1
+mkdir ~/retrobox_led_control  > /dev/null  2>&1
 echo -e "${green}done${nocolor}";
 
 echo -n -e "   --> Downloading Files:           "
 if [ $ani_type = "1" ]
 then
-	wget https://raw.githubusercontent.com/splitti/phoniebox_led_control/master/gpiozero_led_kr.py -qO /home/pi/phoniebox_led_control/gpiozero_led.py > /dev/null  2>&1
+	wget https://raw.githubusercontent.com/connongit/led_driver/master/gpiozero_led_kr.py -qO /home/pi/retrobox_led_control/gpiozero_led.py > /dev/null  2>&1
 else
-	wget https://raw.githubusercontent.com/splitti/phoniebox_led_control/master/gpiozero_led_st.py -qO /home/pi/phoniebox_led_control/gpiozero_led.py > /dev/null  2>&1
+	wget https://raw.githubusercontent.com/connongit/led_driver/master/gpiozero_led_per.py -qO /home/pi/retrobox_led_control/gpiozero_led.py > /dev/null  2>&1
 fi
 echo -e "${green}done${nocolor}";
 
 echo -n -e "   --> Installing Service:          "
-sudo wget https://raw.githubusercontent.com/splitti/phoniebox_led_control/master/service/phoniebox_led_control.service -qO /etc/systemd/system/phoniebox_led_control.service > /dev/null  2>&1
-sudo chown root:root /etc/systemd/system/phoniebox_led_control.service > /dev/null 2>&1
-sudo chmod 644 /etc/systemd/system/phoniebox_led_control.service > /dev/null 2>&1
-sudo systemctl enable phoniebox_led_control > /dev/null 2>&1
+sudo wget https://raw.githubusercontent.com/connongit/led_driver/master/service/retrobox_led_control.service -qO /etc/systemd/system/retrobox_led_control.service > /dev/null  2>&1
+sudo chown root:root /etc/systemd/system/retrobox_led_control.service > /dev/null 2>&1
+sudo chmod 644 /etc/systemd/system/retrobox_led_control.service > /dev/null 2>&1
+sudo systemctl enable retrobox_led_control > /dev/null 2>&1
 echo -e "${green}done${nocolor}";
 
 echo -e ""
@@ -171,15 +171,15 @@ echo -e "///   ${green} ╚═════╝ ╚═════╝ ╚═╝   
 echo -e "///                                                                                                   ///"
 echo -e "/////////////////////////////////////////////////////////////////////////////////////////////////////////"
 echo -e ""
-echo -e "Please edit the file ${red}/home/pi/phoniebox_led_control/gpiozero_led.py${nocolor} to match your GPIO-Configuration!"
+echo -e "Please edit the file ${red}/home/pi/retrobox_led_control/gpiozero_led.py${nocolor} to match your GPIO-Configuration!"
 echo -e ""
 echo -e "CURRENT CONFIGURATION:"
 echo -e "========================="
-echo -e "${yellow}<  PREV-LED     = GPIO 5${nocolor}"
-echo -e "${yellow}|> PLAY-LED     = GPIO 6${nocolor}"
-echo -e "${yellow}>  NEXT-LED     = GPIO 22${nocolor}"
-echo -e "${yellow}+  VOLUP-LED    = GPIO 24${nocolor}"
-echo -e "${yellow}-  VOLDOWN-LED  = GPIO 23${nocolor}"
+echo -e "${yellow}1  = GPIO 5${nocolor}"
+echo -e "${yellow}2  = GPIO 6${nocolor}"
+echo -e "${yellow}3  = GPIO 22${nocolor}"
+echo -e "${yellow}4  = GPIO 24${nocolor}"
+echo -e "${yellow}5  = GPIO 23${nocolor}"
 echo -e ""
 read -n 1 -s -r -p "Press any key to finish installation"
 clear
